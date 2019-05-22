fa2aae2430bf4166440df9cf3d9311f7e1c06a6f
nmeinhold <43660864+NateMeinhold@users.noreply.github.com>
2019-05-21 18:21:07 -0700

    testing

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Rhymesaurus from '@/views/Rhymesaurus'
+// TODO: Import new component
+import AdjForNoun from '@/views/AdjForNoun'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Rhymesaurus',
+      component: Rhymesaurus
+    },
+    {
+      path: '/adjfornoun',
+      name: 'adjfornoun',
+      component: AdjForNoun
+    }, 
+    {
+      path: '/homophones',
+      name: 'homophones',
+     //every time I add component: Homophones everything breaks
+    }
+   
+    // TODO: Add new route definition here.
+  ]
+})
